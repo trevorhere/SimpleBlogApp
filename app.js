ac5f172045d17d301f384703968b545cd918e042
trevorhere <trevoranthonylane@gmail.com>
2017-06-30 18:02:57 +0000

    added authentication

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
 expressSanitizer = require("express-sanitizer"),
 mongoose       = require("mongoose"),
+passport       = require("passport"),
+LocalStrategy  = require("passport-local"),
+User           = require("./models/user"),
 express        = require("express"),
 app            = express();
 
@@ -17,6 +20,19 @@ app.use(methodOverride("_method"));
 app.use(expressSanitizer());
 
 
+//PASSPORT CONGIF
+app.use(require("express-session")({
+    secret: "Go Dodgers!!!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 // MONGOOSE/MODEL CONFIG
 var blogSchema  = new mongoose.Schema({
     title: String,
@@ -27,12 +43,19 @@ var blogSchema  = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
 //INDEX ROUTES
 app.get("/", function(req,res){
    res.redirect("/blogs"); 
 });
 
 app.get("/blogs", function(req,res){
+    
     Blog.find({},function(err, blogs){
         if(err)
         {
@@ -47,12 +70,12 @@ app.get("/blogs", function(req,res){
 });
 
 // NEW/CREATE ROUTE
-app.get("/blogs/new", function(reg,res){
+app.get("/blogs/new", isLoggedIn,function(reg,res){
    res.render("new"); 
 });
 
 //CREATE ROUTE
-app.post("/blogs",function(req,res){
+app.post("/blogs", isLoggedIn, function(req,res){
    //CREATE BLOG
    req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.create(req.body.blog,function(err, newBlog){
@@ -97,7 +120,7 @@ app.get("/blogs/:id/edit",function(req, res){
 });
 
 //UPDATE ROUTE
-app.put("/blogs/:id", function(req, res){
+app.put("/blogs/:id", isLoggedIn,  function(req, res){
     req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
       if(err)
@@ -112,7 +135,7 @@ app.put("/blogs/:id", function(req, res){
 });
 
 //DELETE ROUTE
-app.delete("/blogs/:id", function(req, res){
+app.delete("/blogs/:id", isLoggedIn, function(req, res){
     Blog.findByIdAndRemove(req.params.id, function(err){
       if(err)
       {
@@ -124,6 +147,59 @@ app.delete("/blogs/:id", function(req, res){
       }
     });
  });
+
+
+//AUTHENTICATION ROUTES
+
+//show register form
+app.get("/register", isLoggedIn, function (req,res){
+    res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", isLoggedIn, function(req,res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user){
+        if(err)
+        {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req,res, function(){
+            res.redirect("/blogs");
+        })
+    })
+});
+
+//show login form
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+//handle login logic
+app.post("/login",passport.authenticate("local", 
+{
+    successRedirect: "/blogs",
+    failureRedirect:"/login"
+    
+    }), function(req,res){
+
+});
+
+
+//logout logic 
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/blogs");
+})
+
+function isLoggedIn(req,res,next)
+{
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
